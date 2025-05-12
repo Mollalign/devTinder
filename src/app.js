b66bfdb1475f7a5b180ce1b218e4f42fc25170ed
Mollalign Daniel <molledan69@gmail.com>
2025-05-12 15:08:15 +0200

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user", (req,res) => {
+  res.send({firstName: "Mollalign", lastName: "Daniel"});
+});
 
+app.post("/user", (req, res) => {
+  res.send("Data is Saved successfully in The Database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted!");
+});
 
 
 app.listen(3000, () => {
