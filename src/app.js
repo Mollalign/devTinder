3000417c175d24a5200b2058126bedd74a84be92
Mollalign Daniel <molledan69@gmail.com>
2025-05-18 15:24:50 +0200

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -72,10 +72,29 @@ app.delete("/user", async (req, res) => {
 });
  
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = [
+      "photoUrl", 
+      "about", 
+      "gender", 
+      "age",
+      "skills"
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
+
+    if (!isUpdateAllowed) {
+      res.status(400).send("Updated not allowed")
+    }
+
+    if(data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
     const user = await User.findByIdAndUpdate(userId, data, {
       returnDocument: "after",
       runValidators: true
